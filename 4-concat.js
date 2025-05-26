4c5c65be97a9620eae624b075828683386451b4c
kavoomoses <moseskavoo98@gmail.com>
2025-05-26 13:52:23 +0300

    Update 4-concat.js

--- 4-concat.js
+++ 4-concat.js
@@ -1,7 +1,9 @@
 const args = process.argv.slice(2);
 
-if (args.length >= 2) {
-  console.log(`${args[0]} is ${args[1]}`);
+if (args.length === 0) {
+  console.log('undefined is undefined');
+} else if (args.length === 1) {
+  console.log(`${args[0]} is undefined`);
 } else {
-  console.log('Please provide two arguments.');
+  console.log(`${args[0]} is ${args[1]}`);
 }
