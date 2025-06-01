da2eff0fc4512491f95577437b7f86e9ae92758a
kavoomoses <moseskavoo98@gmail.com>
2025-06-01 23:45:08 +0300

    Update 10-factorial.js

--- 10-factorial.js
+++ 10-factorial.js
@@ -1,1 +1,8 @@
+function factorial(n) {
+  if (isNaN(n)) return 1;
+  if (n <= 1) return 1;
+  return n * factorial(n - 1);
+}
 
+const input = parseInt(process.argv[2]);
+console.log(factorial(input));
